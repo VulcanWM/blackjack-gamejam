3a4071f570fbc1e0db5f84970e94c7f225ccb9e6
Vulcan <logmedha09@gmail.com>
2026-02-28 16:49:23 +0000

    Enhance UI components and styling in App.tsx
    Refactor UI elements for better styling and organization.

--- src/App.tsx
+++ src/App.tsx
@@ -75,10 +75,8 @@ function card_shuffle(n: number, deck: {rank: string, suit: string}[]){
 
 
 function App() {
-    // add tokens to bet
     // work on ui
     // submit
-
     const [seeingInstructions, setSeeingInstructions] = useState<boolean>(true);
     const [seconds, setSeconds] = useState<number>(60);
 
@@ -238,16 +236,19 @@ function App() {
 
 
     return (
-        <>
+        <div>
             {((money <= 0 && !gameInProgress) || seconds <= 0) ?
-                <div className={"bg-[url(/1bg.jpeg)] h-screen bg-cover bg-no-repeat w-screen"}>
-                    <h1 className={"text-9xl font-extrabold"}>YOU DIED!!!</h1>
-                    <p>{money <= 0 ? "You lost all your money" : "You ran out of time."}</p>
+                <div
+                    className="bg-[url(/dead.jpeg)] h-screen w-screen bg-cover bg-no-repeat
+             flex flex-col items-center justify-center text-center p-5"
+                >
+                    <h1 className="text-9xl font-extrabold text-red-500">YOU DIED!!!</h1>
+                    <p className={"text-black"}>{money <= 0 ? "A sea monster ate you up because you lost all your money" : "You ran out of time and a whirpool sucked you up"}</p>
                 </div>
             :
                 <>
                     {seeingInstructions ?
-                        <div className={"bg-[url(/1bg.jpeg)] h-screen bg-cover bg-no-repeat w-screen"}>
+                        <div className={"bg-[url(/1bg.jpeg)] h-screen bg-cover bg-no-repeat w-screen p-5"}>
                             <h1 className={"text-7xl font-extrabold"}>Blackjack (Under the sea)</h1>
                             <h3 className={"text-2xl font-bold"}>How to win</h3>
                             <ul>
@@ -267,21 +268,21 @@ function App() {
                                 <li>If your total is higher than the dealer, you win, get money, and go up a level.</li>
                                 <li>5 wins = Game won. If you lose at any point, instant death straight to level 1. If you run out of money, instant death straight to level one.</li>
                             </ul>
-                            <button onClick={() => (setSeeingInstructions(false))}>Start Game</button>
+                            <button className="bg-blue-500 p-2 rounded-xl hover:bg-blue-400" onClick={() => (setSeeingInstructions(false))}>Start Game</button>
                         </div>
                         :
                         <>
                             {currentLevel == 6 ?
-                                <div className={"h-screen bg-cover bg-no-repeat w-screen bg-[url(/win.jpeg)]"}>
+                                <div className={"h-screen bg-cover bg-no-repeat w-screen bg-[url(/win.jpeg)] p-5"}>
                                     <p className={"text-green-800 text-9xl"}>You won!</p>
                                     <p className={"text-red-500 font-extrabold text-4xl"}>Don't gamble in real life</p>
                                 </div>
                                 :
-                                <div className={`${currentLevel == 1 && "bg-[url(/1bg.jpeg)]"} ${currentLevel == 2 && "bg-[url(/2bg.jpeg)]"} ${currentLevel == 3 && "bg-[url(/3bg.jpeg)]"} ${currentLevel == 4 && "bg-[url(/4bg.jpeg)]"} ${currentLevel == 5 && "bg-[url(/5bg.jpeg)]"} h-screen bg-cover bg-no-repeat w-screen`}>
+                                <div className={`${currentLevel == 1 && "bg-[url(/1bg.jpeg)]"} ${currentLevel == 2 && "bg-[url(/2bg.jpeg)]"} ${currentLevel == 3 && "bg-[url(/3bg.jpeg)]"} ${currentLevel == 4 && "bg-[url(/4bg.jpeg)]"} ${currentLevel == 5 && "bg-[url(/5bg.jpeg)]"} h-screen bg-cover bg-no-repeat w-screen p-5`}>
                                     <h1 className={"font-7xl font-bold"}>Blackjack</h1>
-                                    <p>Seconds remaining: {seconds}</p>
-                                    <p>Level: {currentLevel}</p>
-                                    <p>Money: {money}</p>
+                                    <p>Seconds remaining: <span className={"font-bold text-red-500"}>{seconds}</span></p>
+                                    <p>Level: <span className={"font-bold text-green-500"}>{currentLevel}</span></p>
+                                    <p>Money: <span className={"text-yellow-500 font-bold"}>{money}</span></p>
                                     {messageType == "success" &&
                                         <p className={"text-green-400"}>{message}</p>
                                     }
@@ -296,16 +297,21 @@ function App() {
                                             <p>Your cards are:</p>
                                             <div className="flex flex-row gap-2">
                                                 {userCards.map((card, i) =>
-                                                    <div key={i} className={"h-24 w-16 bg-blue-200 text-black"}>{card.rank}{card.suit}</div>
+                                                    <div
+                                                        key={i}
+                                                        className="h-36 w-24 bg-blue-200 text-black text-2xl flex items-center justify-center text-center"
+                                                    >
+                                                        {card.rank}{card.suit}
+                                                    </div>
                                                 )}
                                             </div>
                                             <p>Dealer's first card is</p>
                                             <div className="flex flex-row gap-2">
-                                                <div className={"h-24 w-16 bg-green-200 text-black"}>{dealerCards[0].rank}{dealerCards[0].suit}</div>
+                                                <div className={"h-36 w-24 bg-green-200 text-black text-2xl flex items-center justify-center text-center"}>{dealerCards[0].rank}{dealerCards[0].suit}</div>
                                             </div>
-                                            <div className="flex flex-row gap-2">
-                                                <button onClick={hit}>Hit</button>
-                                                <button onClick={stand}>Stand</button>
+                                            <div className="flex flex-row gap-2 mt-2">
+                                                <button className="bg-orange-500 p-2 rounded-xl hover:bg-orange-400" onClick={hit}>Hit</button>
+                                                <button className="bg-blue-500 p-2 rounded-xl hover:bg-blue-400" onClick={stand}>Stand</button>
                                             </div>
                                         </div>
                                         :   <div>
@@ -314,7 +320,7 @@ function App() {
                                                     <p>Your cards were:</p>
                                                     <div className="flex flex-row gap-2">
                                                         {userCards.map((card, i) =>
-                                                            <div key={i} className={"h-24 w-16 bg-blue-200 text-black"}>{card.rank}{card.suit}</div>
+                                                            <div key={i} className={"h-36 w-24 bg-blue-200 text-black text-2xl flex items-center justify-center text-center"}>{card.rank}{card.suit}</div>
                                                         )}
                                                     </div>
                                                     {needToSeeDealerCards &&
@@ -322,16 +328,16 @@ function App() {
                                                             <p>Dealer's cards were:</p>
                                                             <div className="flex flex-row gap-2">
                                                                 {dealerCards.map((card, i) =>
-                                                                    <div key={i} className={"h-24 w-16 bg-green-200 text-black"}>{card.rank}{card.suit}</div>
+                                                                    <div key={i} className={"h-36 w-24 bg-green-200 text-black text-2xl flex items-center justify-center text-center"}>{card.rank}{card.suit}</div>
                                                                 )}
                                                             </div>
                                                         </>
                                                     }
                                                 </>
                                             }
-                                            <p>Bet:</p>
-                                            <input type="number" value={bet} onChange={e => setBet(parseInt(e.target.value))}></input>
-                                            <button onClick={startGame}>Bet</button>
+                                            <br/>
+                                            <input className="bg-blue-300 rounded-xl text-black p-2 mr-4" type="number" value={bet} onChange={e => setBet(parseInt(e.target.value))}></input>
+                                            <button className="bg-blue-500 p-2 rounded-xl hover:bg-blue-400" onClick={startGame}>Bet</button>
                                         </div>
                                     }
                                 </div>
@@ -340,7 +346,8 @@ function App() {
                     }
                 </>
             }
-        </>
+        </div>
+
     )
 }
 
